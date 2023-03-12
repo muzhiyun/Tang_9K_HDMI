3646b3beac2faf1894b7e8787f9f7291bf9fb8d2
muzhiyun <my616422@126.com>
2023-03-12 16:36:17 +0800

    Add ASAMPLE

--- top/top.sv
+++ top/top.sv
@@ -10,7 +10,7 @@ module top (
     //output logic [2:0] tmds,
     //output logic tmds_clock
 
-    output [2:0] led2,  
+    output [5:0] led,  
 
     // output signals
     output       tmds_clk_n,
@@ -28,7 +28,6 @@ wire clk_pixel;  // 720p pixel clock: 74.25 Mhz
 wire clk_pixel_x5;  // 5x pixel clock: 371.25 Mhz
 wire pll_lock;
 
-logic clk_audio;
 //logic reset;
 
 Gowin_rPLL u_pll(
@@ -42,13 +41,14 @@ Gowin_CLKDIV u_div_5(
         .resetn(sys_resetn & pll_lock) //input resetn
 );
 
+/********************************Test CLK Start****************************************/
 reg [32:0] cnt_clk;              
 reg [32:0] cnt_pixel;  
-reg [32:0] cnt_pixel5;              
+reg [32:0] cnt_pixel5 = 0;              
 
-assign led2[0] = (cnt_clk < 26'd500) ? 2'b1 : 2'b0;
-assign led2[2] = (cnt_pixel < 26'd500) ? 2'b1 : 2'b0;
-assign led2[1] = (cnt_pixel5 < 26'd2500_000) ? 2'b1 : 2'b0;
+//assign led[0] = (cnt_clk < 26'd500) ? 1'b1 : 1'b0;
+assign led[1] = (cnt_pixel < 26'd500) ? 1'b1 : 1'b0;
+assign led[2] = (cnt_pixel5 < 26'd500) ? 1'b0 : 1'b1;
 
 
 always @ (posedge sys_clk or negedge sys_resetn) begin
@@ -69,28 +69,75 @@ always @ (posedge clk_pixel or negedge sys_resetn) begin
         cnt_pixel <= 26'd0;           
 end
 
-always @ (posedge clk_pixel_x5 or negedge sys_resetn) begin
-    if(!sys_resetn)                  
-        cnt_pixel5 <=26'd0;
-    else if(cnt_pixel5 < 26'd5000_000)
+
+always @ (posedge clk_pixel_x5) begin
+    if(cnt_pixel5 < 26'd1000)
         cnt_pixel5 <= cnt_pixel5 + 1'b1;      
     else
         cnt_pixel5 <= 26'd0;
 end
+/********************************Test CLK End****************************************/
+
+// audio stuff
+logic [10:0] audio_divider;
+logic clk_audio;
+
+localparam AUDIO_RATE=48000;
+localparam CLKFRQ = 74250;
+
+always_ff@(posedge clk_pixel) 
+begin
+    if (audio_divider != CLKFRQ * 1000 / AUDIO_RATE / 2 - 11'd1) 
+        audio_divider++; //generated from clk_pixel 27.0000MHz/281=48042,70Hz ; 27.0000MHz/306=44117,64Hz
+    else begin 
+        clk_audio <= ~clk_audio; 
+        audio_divider <= 0; 
+    end
+end
 
 logic [15:0] audio_sample_word [1:0] = '{16'd0, 16'd0};
-always @(posedge clk_audio)
-  audio_sample_word <= '{audio_sample_word[1] + 16'd1, audio_sample_word[0] - 16'd1};
 
-logic [23:0] rgb = 24'h00ff00;
-//24'hffffff;   // R G B
+//Audio Test Data
+always @(posedge clk_audio)
+  //audio_sample_word <= {16'haaaa,16'haaaa};
+  audio_sample_word <= '{audio_sample_word[1] + 16'h1111, audio_sample_word[0] - 16'h1111};
+
+
+/********************************Test CLK Start****************************************/
+reg [32:0] cnt_audio_clk;  
+
+assign led[3] = (cnt_audio_clk < 26'd500) ? 1'b0 : 1'b1;
+
+
+always @ (posedge clk_audio or negedge sys_resetn) begin
+    if(!sys_resetn)                  
+        cnt_audio_clk <=26'd0;
+    else if(cnt_audio_clk < 26'd1000)
+        cnt_audio_clk <= cnt_audio_clk + 1'b1;    
+    else
+        cnt_audio_clk <= 26'd0;     
+end
+/********************************Test CLK End****************************************/
 
 logic [9:0] cx, cy, screen_start_x, screen_start_y, frame_width, frame_height, screen_width, screen_height;
+
+logic [23:0] rgb = 24'hff0000;   //24'hffffff;   // R G B
+
+//Video Test Pattern
 // Border test (left = red, top = green, right = blue, bottom = blue, fill = black)
 always @(posedge clk_pixel)
-  rgb <= {cx == 0 ? ~8'd0 : 8'd0, cy == 0 ? ~8'd0 : 8'd0, cx == screen_width - 1'd1 || cy == screen_width - 1'd1 ? ~8'd0 : 8'd0};
+//  rgb <= {cx == 0 ? ~8'd0 : 8'd0, cy == 0 ? ~8'd0 : 8'd0, cx == screen_width - 1'd1 || cy == screen_width - 1'd1 ? ~8'd0 : 8'd0};
+    if (cy < 240 )
+       rgb = 24'hff0000;
+    else if (cy < 480 )
+       rgb = 24'h00ff00;
+    else
+       rgb = 24'h0000ff; 
 
-// 640x480 @ 59.94Hz
+
+
+
+// 1280x720 @ 59.94Hz
 hdmi #(.VIDEO_ID_CODE(4), .VIDEO_REFRESH_RATE(59.94), .AUDIO_RATE(48000), .AUDIO_BIT_WIDTH(16)) hdmi(
   .clk_pixel_x5(clk_pixel_x5),
   .clk_pixel(clk_pixel),
